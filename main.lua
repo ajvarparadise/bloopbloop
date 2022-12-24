93813a3df219043587278793b65a739ebc1bfc5b
ajvarparadise <mail.admir.husovic@gmail.com>
2022-12-24 14:33:09 +0100

    clean up

--- main.lua
+++ main.lua
@@ -4,37 +4,78 @@
 -- [ ] flytta gubbe till höger
 -- [x] lyssna på io för instruktioner 
 -- [x] ta emot input på något sätt? event? keyboard value
-
 -- [ ] TODO:
-
-
 local utf8 = require "utf8"
 
-function setBackground()
-    love.graphics.setBackgroundColor(110/225,110/225,110/225,100/100)
+grey = {110 / 225, 110 / 225, 110 / 225, 100 / 100}
+
+white = {225 / 225, 225 / 225, 225 / 225, 100 / 100}
+
+function love.draw()
+    userInput()
 end
 
 function love.load()
-    text = ""
+    resetScreen()
+    placeholder = "type something here -->"
+    text = placeholder
+end
+
+function love.keypressed(key)
+    if key == "return" then
+        translateInputToAction(text)
+        text = ""
+    end
+
+    if key == "backspace" and string.len(text) > 0 then
+        deleteCharacterFromString(text)
+    end
+    if key == "rctrl" then -- set to whatever key you want to use
+        debug.debug()
+    end
 end
 
 function love.textinput(t)
--- only allow text character 
-   text = text .. t
+    if (t:match("%s")) then
+        -- Improper characters detected.
+    else
+        placeholder = ""
+        text = text .. t
+    end
 end
 
+function resetScreen()
+    setBackground()
+    text = ""
+end
 
-function love.keypressed(key)
-    if key == "return" 
-        then 
-            action = text 
-            text = ""
-    end
+function deleteCharacterFromString(str)
+    local offset = getStringLength(text) - 1
+    text = string.sub(text, 1, utf8.offset(text, offset))
+end
 
-    if key == "backspace" and string.len(text) > 0
-        then 
-            text = string.sub(text, 1, utf8.offset(text,getStringLength(text) ) - 1)
-        end
+function setBackground()
+    love.graphics.setBackgroundColor(grey)
+end
+
+function userInput()
+    local consoleHeight = 50
+    consoleXStart = love.graphics.getHeight() - consoleHeight
+    drawConsole(consoleHeight, consoleXStart)
+    printUserInput(consoleXStart + (consoleHeight / 2) - 10)
+end
+
+function drawConsole(consoleHeight, consoleXStart)
+    local consoleYEnd = 0
+    local consoleWidth = love.graphics.getWidth()
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle('fill', 0, consoleXStart, consoleWidth, consoleHeight)
+
+end
+
+function printUserInput(consoleXStart)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.printf(text, 10, consoleXStart, love.graphics.getWidth())
 end
 
 function getStringLength(str)
@@ -43,13 +84,17 @@ function getStringLength(str)
 end
 
 function makeItRain()
- -- easy effect for kids
+    -- easy effect for kids
 end
 
-function love.draw()
-    setBackground()
-    love.graphics.printf(text, 0, 0, love.graphics.getWidth())
-    if action == "love.jump()" 
-        then love.graphics.printf("juuuuump", 20, 20, love.graphics.getWidth()) 
+function handleJump()
+    love.graphics.setBackgroundColor(white)
+end
+
+function translateInputToAction(action)
+    if action == "jump()" then
+        handleJump()
+    else
+        resetScreen()
     end
 end
